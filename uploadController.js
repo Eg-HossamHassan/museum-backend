a57c8308ce9ab4b65be9ec220644d5382a998111
Eg-HossamHassan <igsizgbsozv@gmail.com>
2026-03-14 06:19:11 +0200

    Delete uploadController.js

--- uploadController.js
+++
@@ -1,6 +0,0 @@
-// controllers/uploadController.js
-exports.uploadImage = (req, res) => {
-  if (!req.file) return res.status(400).json({ msg: "No file uploaded" });
-
-  res.json({ imageUrl: `/uploads/${req.file.filename}` });
-};
