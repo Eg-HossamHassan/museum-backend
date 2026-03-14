c7838c229d687c8b7780bcee83982aa9ed28d6f3
Eg-HossamHassan <igsizgbsozv@gmail.com>
2026-03-14 22:24:28 +0200

    Refactor server.js to include new routes and Swagger setup
    Added favorite and upload routes, and updated Swagger configuration.

--- server.js
+++ server.js
@@ -1,24 +1,58 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
+const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const artifactRoutes = require("./routes/artifactRoutes");
-const app = express();
-const connectDB = require("./config/db");
+const favoriteRoutes = require("./routes/favoriteRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const translateRoutes = require("./routes/translateRoutes");
+
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+
+const app = express();
+
+// connect database
 connectDB();
+
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth",authRoutes);
-app.use("/api/artifacts",artifactRoutes);
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("MongoDB connection failed:", err.message));
 
-app.get("/", (req,res)=>{
- res.send("Museum API Running");
+// Swagger config
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Museum API",
+      version: "1.0.0",
+      description: "Museum backend API documentation"
+    },
+    servers: [
+      {
+        url: "https://museum-backend-production-3150.up.railway.app"
+      }
+    ]
+  },
+  apis: ["./routes/*.js"]
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/artifacts", artifactRoutes);
+app.use("/api/favorites", favoriteRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/translate", translateRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Museum API Running");
 });
 
 const PORT = process.env.PORT || 5000;
